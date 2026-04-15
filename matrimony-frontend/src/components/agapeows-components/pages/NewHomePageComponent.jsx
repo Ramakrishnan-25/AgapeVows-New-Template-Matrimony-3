26a54ae4b81dfec02130cca55dda13872de45311
Ramakrishnan <rramakrishnan142@gmail.com>
2026-04-15 10:48:19 +0530

    Adjusted homepage sections wrapper to have less vertical gap and slightly more horizontal padding/inward constraint

--- matrimony-frontend/src/components/agapeows-components/pages/NewHomePageComponent.jsx
+++ matrimony-frontend/src/components/agapeows-components/pages/NewHomePageComponent.jsx
@@ -29,7 +29,7 @@ const NewHomePageComponent = () => {
       <div className="pt-16">
         <HeroSearchSection />
         
-        <div className="flex flex-col gap-12 lg:gap-20 md:py-10 overflow-hidden">
+        <div className="flex flex-col gap-6 lg:gap-8 py-4 sm:py-8 overflow-hidden max-w-[1400px] mx-auto px-4 sm:px-8 lg:px-12">
           <TrustBrandsSection />
           <QuickAccessSection />
           <AboutWelcomeSection />
